29d436aa89625ae99fcc11a58d6574fcf89fd109
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-09 10:09:51 +0100

    Picking both options and cli params into paths variable

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ var transform = transformTools.makeRequireTransform(
         jsFilesOnly: true
     },
     function(args, opts, cb) {
+        var paths = Object.keys(opts.config)
+                        .filter(function(opts_key) { return opts_key.length == 1 })
+                        .reduce(function(coll, opts_key) { 
+                            return coll.concat(opts.config[opts_key])
+                        },[])
+        if (paths.length == 0) { return cb(null) }
         resolved = resolve.sync(args[0], {
             basedir: path.dirname(opts.file),
-            moduleDirectory: ['node_modules'].concat(opts.config)
+//            basedir: __dirname,
+            moduleDirectory: ['node_modules'].concat(paths)
         });
 
         return cb(null, "require('"+resolved+"')");
